04c99cd7b42ddb8817c8ff050c17ef162efb6822
achen2610 <hoainam@MacBook-Pro.local>
2016-01-01 13:14:18 +0700

    Create new project
    New project

---
+++ DictionaryLabo/DictionaryLabo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DictionaryLabo
+//
+//  Created by Pham Hoai Nam on 1/1/16.
+//  Copyright © 2016 Duy Hung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
